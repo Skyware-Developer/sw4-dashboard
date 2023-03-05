3bd80821ac89555ac8f2e710523c25551c76a5f0
Gabriel Bateca <gabriel.bateca@outlook.com>
2023-03-05 17:03:39 -0500

    feat: add firebase with login

---
+++ src/app/login/page.js
@@ -0,0 +1,71 @@
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+export default function Login() {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDahBmoVxjj7aeJwU2wkpayU71Pnmpvrw4",
+    authDomain: "sw4-udem.firebaseapp.com",
+    projectId: "sw4-udem",
+    storageBucket: "sw4-udem.appspot.com",
+    messagingSenderId: "544450645227",
+    appId: "1:544450645227:web:413a284578d5ef0ac682ae",
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (auth, email, password) => signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    alert('Login exitoso');
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    alert('Error');
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(`error code ${errorCode} message: ${errorMessage}`);
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Email is: ${email} and password is: ${password}`);
+    signIn(getAuth(), email, password);
+  };
+
+  return (
+    <div className={styles.loginContainer}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
